ca9733bf5242a242fe7f63a7af29117242c6292c
Sai Suresh <saisuresh8179@gmail.com>
2023-07-17 11:52:35 +0530

    version-1 code for calculator in java

---
+++ calculator.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Operations
+{
+    int add(int a,int b)
+    {
+        return (a+b);
+    }
+    int sub(int a,int b)
+    {
+        return (a-b);
+    }
+}
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        Operations o=new Operations();
+        System.out.println(o.add(a,b));
+        System.out.println(o.sub(a,b));
+    }
+}
